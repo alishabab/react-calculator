55c6aac604f41939e58920337bdfddbd603048e3
Shabab <shababsaifi@gmail.com>
2020-09-10 18:03:01 +0530

    added styles to Button and ButtonPanel components

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classes from './Button.module.css';
 
-const Button = ({ buttonName }) => (
-  <div>
-    <button type="button">
+const Button = ({ buttonName, wide, color }) => (
+  <div className={wide ? classes.wide : classes.Button}>
+    <button type="button" className={color ? classes.color : null}>
       { buttonName }
     </button>
   </div>
@@ -11,6 +12,13 @@ const Button = ({ buttonName }) => (
 
 Button.propTypes = {
   buttonName: PropTypes.string.isRequired,
+  wide: PropTypes.bool,
+  color: PropTypes.bool,
+};
+
+Button.defaultProps = {
+  wide: false,
+  color: false,
 };
 
 export default Button;
--- src/components/ButtonPanel/ButtonPanel.js
+++ src/components/ButtonPanel/ButtonPanel.js
@@ -1,39 +1,36 @@
 import React from 'react';
 import Button from '../Button/Button';
-
-const styles = {
-  display: 'flex',
-};
+import classes from './ButtonPanel.module.css';
 
 const ButtonPanel = () => (
   <div>
-    <div style={styles}>
-      <Button buttonName="AC" />
-      <Button buttonName="+/-" />
-      <Button buttonName="%" />
+    <div className={classes.row}>
+      <Button buttonName="AC" color />
+      <Button buttonName="+/-" color />
+      <Button buttonName="%" color />
       <Button buttonName="÷" />
     </div>
-    <div style={styles}>
-      <Button buttonName="7" />
-      <Button buttonName="8" />
-      <Button buttonName="9" />
+    <div className={classes.row}>
+      <Button buttonName="7" color />
+      <Button buttonName="8" color />
+      <Button buttonName="9" color />
       <Button buttonName="X" />
     </div>
-    <div style={styles}>
-      <Button buttonName="4" />
-      <Button buttonName="5" />
-      <Button buttonName="6" />
+    <div className={classes.row}>
+      <Button buttonName="4" color />
+      <Button buttonName="5" color />
+      <Button buttonName="6" color />
       <Button buttonName="-" />
     </div>
-    <div style={styles}>
-      <Button buttonName="1" />
-      <Button buttonName="2" />
-      <Button buttonName="3" />
+    <div className={classes.row}>
+      <Button buttonName="1" color />
+      <Button buttonName="2" color />
+      <Button buttonName="3" color />
       <Button buttonName="+" />
     </div>
-    <div style={styles}>
-      <Button buttonName="0" />
-      <Button buttonName="." />
+    <div className={classes.row}>
+      <Button buttonName="0" wide color />
+      <Button buttonName="." color />
       <Button buttonName="=" />
     </div>
   </div>
