9d5568f4055610c0856cba4b24167300088bf701
Shabab <shababsaifi@gmail.com>
2020-09-11 14:35:32 +0530

    updated operate method

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,8 +1,13 @@
+import operate from './operate';
+
 const calculate = (calculator, buttonName) => {
   const newCalculator = { ...calculator };
   if (buttonName === '+/-') {
     newCalculator.total *= -1;
     newCalculator.next *= -1;
+  }
+  if (buttonName === '+') {
+    newCalculator.total = operate(calculator.total, calculator.next, calculator.operator);
   }
   return newCalculator;
 };
--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,12 +1,26 @@
 import Big from 'big.js';
 
 const operate = (numberOne, numberTwo, operation) => {
-  const a = new Big(numberOne);
-  const b = new Big(numberTwo);
+  const a = Big(numberOne);
+  const b = Big(numberTwo);
+
   if (operation === '+') {
     return a.plus(b);
   }
-  return 0;
+
+  if (operation === '-') {
+    return a.minus(b);
+  }
+
+  if (operation === 'X') {
+    return a.times(b);
+  }
+
+  if (operation === '÷') {
+    return a.div(b);
+  }
+
+  return Big(numberOne).times(0.01);
 };
 
 export default operate;
